4600041b36d62c8b57187308fc02e6252796f301
JungHyoMoon <dsinhome70@gmail.com>
2021-06-23 15:09:49 +0900

    movement error fixed.

--- main.js
+++ main.js
@@ -3,28 +3,29 @@ const ctx = canvas.getContext("2d");
 
 let x = canvas.width / 2;
 let y = canvas.height / 2;
-const radius = 10;
+let vx = 4;
+let vy = 3;
+const radius = 5;
 
 const ball = (x, y, radius) => {
     ctx.beginPath();
     ctx.arc(x, y, radius, 0, Math.PI * 2);
     ctx.closePath();
-    ctx.fillStyle = "blue";
+    ctx.fillStyle = "#fbc531";
     ctx.fill();
+    if (x + radius > canvas.width || x - radius < 0) {
+        vx = -vx;
+    }
+    if (y + radius > canvas.height || y - radius < 0) {
+        vy = -vy;
+    }
 };
 
 const draw = () => {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    let speed = 3;
-    if (x + radius > canvas.width || x - radius < 0) {
-        speed = -speed;
-    }
-    if (y + radius > canvas.width || y - radius < 0) {
-        speed = -speed;
-    }
     ball(x, y, radius);
-    x += speed;
-    y += speed;
+    x += vx;
+    y += vy;
     window.requestAnimationFrame(draw);
 };
 
